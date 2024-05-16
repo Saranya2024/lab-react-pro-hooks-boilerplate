1ee3029e59c03fab9b6353f2b97f88f5061f5c4c
Saranya2024 <159130151+Saranya2024@users.noreply.github.com>
2024-05-16 19:01:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React,{useEffect,useState,useMemo} from 'react';
+
+import React,{useCallback, useEffect,useState, useMemo} from 'react';
 import './App.css';
 
 // Do not change this
@@ -6,28 +7,34 @@ const LARGE_NUMBER = 1000000000;
 
 function App() {
 
+
   const [value, setValue] = useState(0);
   const [dark, setTheme] = useState(true);
   const [themeName, setThemeName] = useState("dark");
   const [currentList, setList] = useState([]);
-  
+
+
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const delayFunction = useMemo(()=> {
+  const delayFunction = ()=> {
     console.log("Delay Function Ran")
     for(let index=0; index<LARGE_NUMBER; index++){};
     return value+2;
   
-  },[value])
+  }
 
   // should not change the LOGIC inside this function - you can make changes to the function but logic should NOT change
-  const testFunction = useMemo(()=>{
+  const testFunction = useCallback(()=>{
     return [value*3 ,value*4]
   },[value])
+
+  
 
   // should not change this
   useEffect(()=>{
     console.log("Callback Function was called")
   },[testFunction])
+
+
 
   useEffect(()=>{
     if(dark){
@@ -37,6 +44,7 @@ function App() {
       setThemeName("light")
     }
   },[dark])
+
 
   const handleClick = ()=>{
     setTheme(!dark);
@@ -61,7 +69,7 @@ function App() {
       <h1 >{value}</h1>
       <button onClick={handleChangeValue}>Change Value</button>
       <button onClick={handleList}>Show List</button>
-      <h2>{delayFunction}</h2>
+      <h2>{useMemo(() => delayFunction(), [value])}</h2>
       <div>
         {currentList.map((item,index)=>{
           return <h2 key={index}>{item}</h2>
